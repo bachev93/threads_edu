9b735e556324677f5c56ddbaf9c7b9a70d722d19
Sergey Bachev <bachev93@mail.ru>
2023-06-21 23:39:17 +0300

    add simple daemon as systemd service

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -1,7 +1,13 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 int main() {
-    std::cout << "It's daemon programm" << std::endl;
+    while (true) {
+        std::cout << "It's daemon programm" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "After waiting" << std::endl;
+    }
 
     return 0;
 }
